75dd481256db161fd7e244212d2bf1e554adf291
Iván García <ivan.garcia.ext@diagroup.com>
2021-06-21 16:27:12 +0200

    Delete cuestas

--- app/src/main/java/com/ivione93/hojames/ui/trainings/AdapterCuestas.java
+++ app/src/main/java/com/ivione93/hojames/ui/trainings/AdapterCuestas.java
@@ -3,6 +3,7 @@ package com.ivione93.hojames.ui.trainings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,10 +11,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.ivione93.hojames.R;
 import com.ivione93.hojames.model.Cuestas;
 
 public class AdapterCuestas extends FirestoreRecyclerAdapter<Cuestas, AdapterCuestas.CuestasViewHolder> {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public AdapterCuestas(@NonNull FirestoreRecyclerOptions<Cuestas> options) {
         super(options);
@@ -23,6 +27,12 @@ public class AdapterCuestas extends FirestoreRecyclerAdapter<Cuestas, AdapterCue
     protected void onBindViewHolder(@NonNull CuestasViewHolder holder, int position, @NonNull Cuestas model) {
         holder.showTypeCuestas.setText(model.type);
         holder.showTimesCuestas.setText(String.valueOf(model.times));
+
+        holder.ibDeleteCuesta.setOnClickListener(v -> {
+            db.collection("cuestas").document(model.id).delete();
+            notifyItemRangeChanged(position, getItemCount());
+            notifyItemRemoved(position);
+        });
     }
 
     @NonNull
@@ -35,11 +45,13 @@ public class AdapterCuestas extends FirestoreRecyclerAdapter<Cuestas, AdapterCue
     class CuestasViewHolder extends RecyclerView.ViewHolder {
 
         TextView showTypeCuestas, showTimesCuestas;
+        ImageButton ibDeleteCuesta;
 
         public CuestasViewHolder(@NonNull View itemView) {
             super(itemView);
             showTypeCuestas = itemView.findViewById(R.id.showTypeCuestas);
             showTimesCuestas = itemView.findViewById(R.id.showTimesCuestas);
+            ibDeleteCuesta = itemView.findViewById(R.id.ibDeleteCuesta);
         }
     }
 }

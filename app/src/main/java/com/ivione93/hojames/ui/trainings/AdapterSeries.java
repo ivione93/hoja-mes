e45fb5179762ac45277e7b035e00aaf894fcde4b
Iván García <ivan.garcia.ext@diagroup.com>
2021-06-21 15:56:10 +0200

    Delete series

--- app/src/main/java/com/ivione93/hojames/ui/trainings/AdapterSeries.java
+++ app/src/main/java/com/ivione93/hojames/ui/trainings/AdapterSeries.java
@@ -3,6 +3,7 @@ package com.ivione93.hojames.ui.trainings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,10 +11,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.ivione93.hojames.R;
 import com.ivione93.hojames.model.Series;
 
 public class AdapterSeries extends FirestoreRecyclerAdapter<Series, AdapterSeries.SeriesViewHolder> {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public AdapterSeries(@NonNull FirestoreRecyclerOptions<Series> options) {
         super(options);
@@ -23,6 +27,12 @@ public class AdapterSeries extends FirestoreRecyclerAdapter<Series, AdapterSerie
     protected void onBindViewHolder(@NonNull SeriesViewHolder holder, int position, @NonNull Series model) {
         holder.showDistanceSerie.setText(model.distance + " m");
         holder.showTimeSerie.setText(model.time);
+
+        holder.ibDeleteSerie.setOnClickListener(v -> {
+            db.collection("series").document(model.id).delete();
+            notifyItemRangeChanged(position, getItemCount());
+            notifyItemRemoved(position);
+        });
     }
 
     @NonNull
@@ -35,11 +45,13 @@ public class AdapterSeries extends FirestoreRecyclerAdapter<Series, AdapterSerie
     class SeriesViewHolder extends RecyclerView.ViewHolder {
 
         TextView showDistanceSerie, showTimeSerie;
+        ImageButton ibDeleteSerie;
 
         public SeriesViewHolder(@NonNull View itemView) {
             super(itemView);
             showDistanceSerie = itemView.findViewById(R.id.showDistanceSerie);
             showTimeSerie = itemView.findViewById(R.id.showTimeSerie);
+            ibDeleteSerie = itemView.findViewById(R.id.ibDeleteSerie);
         }
     }
 }

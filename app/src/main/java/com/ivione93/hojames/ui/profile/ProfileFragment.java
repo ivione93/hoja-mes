e33eda9237a321db9fed493d9bdbdc6c60c424ef
Ivan Garcia <i.garcia.ext@diagroup.com>
2021-10-29 11:02:37 +0200

    Navegacion directa a ultimo entreno y competicion

--- app/src/main/java/com/ivione93/hojames/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ivione93/hojames/ui/profile/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
@@ -56,6 +59,8 @@ public class ProfileFragment extends Fragment {
     TextView title_training, last_training_date, title_time, title_distance, title_partial, last_training_time, last_training_distance, last_training_partial;
     TextView tvIndicadorSeries, tvIndicadorCuestas, tvIndicadorFartlek, tvIndicadorGym;
     ImageView ivIndicadorSeries, ivIndicadorCuestas, ivIndicadorFartlek, ivIndicadorGym, ivIndicadorObserves;
+
+    CardView lastTrainingCV, lastCompetitionCV;
 
     String email;
     String dateSelected = Utils.toString(new Date());
@@ -200,6 +205,9 @@ public class ProfileFragment extends Fragment {
         ivIndicadorGym = root.findViewById(R.id.ivIndicadorGym);
 
         ivIndicadorObserves = root.findViewById(R.id.ivIndicadorObserves);
+
+        lastTrainingCV = root.findViewById(R.id.last_training);
+        lastCompetitionCV = root.findViewById(R.id.last_competition);
     }
 
     private void getLastCompetition(String email) {
@@ -211,6 +219,13 @@ public class ProfileFragment extends Fragment {
                 progressDialog.dismiss();
                 for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
                     if (documentSnapshot.exists()) {
+                        lastCompetitionCV.setOnClickListener(v -> {
+                            Intent newCompetition = new Intent(getContext(), NewCompetitionActivity.class);
+                            newCompetition.putExtra("isNew", false);
+                            newCompetition.putExtra("idCompetition", documentSnapshot.get("id").toString());
+                            newCompetition.putExtra("email", email);
+                            startActivity(newCompetition);
+                        });
                         last_competition_name.setText(documentSnapshot.get("name").toString());
                         last_competition_place.setText(documentSnapshot.get("place").toString());
                         last_competition_date.setText(Utils.toString((Timestamp) documentSnapshot.get("date")));
@@ -237,6 +252,16 @@ public class ProfileFragment extends Fragment {
                         progressDialog.dismiss();
                         for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
                             if (documentSnapshot.exists()) {
+                                lastTrainingCV.setOnClickListener(v -> {
+                                    Log.d("PROFILE", "Ultimo entreno");
+
+                                    Intent newTraining = new Intent(getContext(), ViewTrainingActivity.class);
+                                    newTraining.putExtra("isNew", false);
+                                    newTraining.putExtra("idTraining", documentSnapshot.get("id").toString());
+                                    newTraining.putExtra("email", email);
+                                    startActivity(newTraining);
+                                });
+
                                 last_training_distance.setText(documentSnapshot.get("distance").toString() + " kms");
                                 last_training_time.setText(Utils.getFormattedTime(documentSnapshot.get("time").toString()) + " min");
                                 last_training_date.setText(Utils.toString((Timestamp) documentSnapshot.get("date")));

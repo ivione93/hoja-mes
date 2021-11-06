566b16cf0f4217d5fd34c9bbaa99181d0b8292b5
Ivan Garcia <i.garcia.ext@diagroup.com>
2021-11-06 21:51:18 +0100

    Colores

--- app/src/main/java/com/ivione93/hojames/SplashScreenActivity.java
+++ app/src/main/java/com/ivione93/hojames/SplashScreenActivity.java
@@ -37,6 +37,6 @@ public class SplashScreenActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), InformationActivity.class);
             startActivity(intent);
             finish();
-        }, 3000);
+        }, 2500);
     }
 }

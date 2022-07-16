363ab2144ca7e788f45e9ad99bc4006ca0b68fa5
Iván García <ivgarciag@yahoo.es>
2022-07-16 16:37:10 +0200

    Refactor item_trainings.xml

--- app/src/main/java/com/ivione93/hojames/ui/trainings/AdapterTrainings.java
+++ app/src/main/java/com/ivione93/hojames/ui/trainings/AdapterTrainings.java
@@ -42,7 +42,7 @@ public class AdapterTrainings extends RecyclerView.Adapter<AdapterTrainings.View
     @NonNull
     @Override
     public AdapterTrainings.ViewHolderTraining onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_training_series, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_training, parent, false);
         return new ViewHolderTraining(view);
     }
 
